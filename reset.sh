8ccef3f9b58355e9a42021bc3aa9bcb5668a82c7
Ethan Mosbaugh <ethan@replicated.com>
2023-03-01 04:36:30 +0000

    stuff

--- reset.sh
+++ reset.sh
@@ -5,16 +5,16 @@ set -euo pipefail
 # arguments
 
 DEBUG=${DEBUG:-false}
-BUILD_DIR=${BUILD_DIR:-./build}
 BIN_DIR=${BIN_DIR:-./bin}
+CONFIG_DIR=${CONFIG_DIR:-./build}
 
 source src/common.sh
 
 function reset() {
-    if [ ! -f "$BUILD_DIR"/cluster.yaml ]; then
+    if [ ! -f "$CONFIG_DIR"/cluster.yaml ]; then
         bail "no cluster config found, skipping reset"
     fi
-    "$BIN_DIR"/k0sctl reset -c "$BUILD_DIR"/cluster.yaml
+    "$BIN_DIR"/k0sctl reset -c "$CONFIG_DIR"/cluster.yaml
 }
 
 reset "$@"
